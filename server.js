88fdba53fc7bf1f57a9c9ddc6e39707adb490d12
teambashify <116701118+teambashify@users.noreply.github.com>
2023-03-01 22:40:00 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
+// import pkg from "./library/header"
+const express = require("express");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const client = new Client({
+  puppeteer: {
+    headless: true,
+    args: [ "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-accelerated-2d-canvas",
+    "--no-first-run",
+    "--no-zygote",
+    "--single-process", // <- this one doesn't works in Windows
+    "--disable-gpu",],
+
+  },
+  authStrategy: new LocalAuth({
+    clientId: "client-one",
+  }),
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+client.initialize();
+
+client.on("authenticated", (session) => {
+  console.log("WHATSAPP WEB => Authenticated");
+});
+
+
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", async () => {
+  console.log("WhatsApp is ready");
+});
+
+client.on("message", async (message) => {
+ 
+  
+});
+
+
+const port = process.env.PORT || 8000;
+app.listen(port, function () {
+  console.log("App is running on*:" + port);
 });
